0821d6151e624c60e8f77f839d424039169110e6
Marin Aleksey <asmadews@gmail.com>
2016-03-15 01:05:09 +0300

    README updates && some PEP8 fixes

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # graphite2opentsdb
-SImple Graphite to opentsdb data loader
+Very simple Graphite to OpenTSDB data loader
+        
+## Install
+
+    pip install -r requirements.txt
+ 
+## Use
+
+Usage: graphite2opentsdb.py <command> <args>
+
+    Supported commands:
+
+    - list:
+        Simply lists available metrics
+
+    - get <graphite.metric.name> [as <target_name>] tag1=value1 [tagN=valueN]:
+        Get Graphite metric <graphite.metric.name> data and return it in OpenTSDB import format, i.e:
+            <graphite.metric.name> <unix_time> <value> <tags>
+        if used with "as <target_name>", graphite metric name will replaced
+
+        Example:
+        > get server.temperature.temp1 as temp1 entity=Obj100
--- graphite2opentsdb.py
+++ graphite2opentsdb.py
@@ -3,39 +3,42 @@ import requests
 import sys
 from requests.exceptions import ConnectionError
 
-import saymon.settings
+import settings
 
-USAGE='''
+USAGE = '''
 Usage: %s <cmd> <args>
 
     Supported commands:
 
     - list:
-        Simply lists avalilable metrics
+        Simply lists available metrics
 
-    - get <graphite.metrica.name> [as <opentsdb_name>] tag1=value1 [tagN=valueN]:
-        Get Graphite metrica <graphite.metrica.name> data and return it in opentsdb format, i.e:
-            <opentsdb_name> <unixtime> <value> <tags>
-        if used with "as <opentsdb_name>", graphite metrica name will replaced
+    - get <graphite.metric.name> [as <target_name>] tag1=value1 [tagN=valueN]:
+        Get Graphite metric <graphite.metric.name> data and return it in OpenTSDB import format, i.e:
+            <graphite.metric.name> <unix_time> <value> <tags>
+        if used with "as <target_name>", graphite metric name will replaced
 
         Example:
-        > get server.temperature.temp1 as temp1 entiry=Obj100
-
+        > get server.temperature.temp1 as temp1 entity=Obj100
 ''' % sys.argv[0]
 
 
 def make_graphite_url(path):
-    graphite_host = saymon.settings.GRAPHITE_PORT == 80 and saymon.settings.GRAPHITE_HOST or "%s:%s" % (
-        saymon.settings.GRAPHITE_HOST, saymon.settings.GRAPHITE_PORT
+    graphite_host = settings.GRAPHITE_PORT == 80 and settings.GRAPHITE_HOST or "%s:%s" % (
+        settings.GRAPHITE_HOST, settings.GRAPHITE_PORT
     )
     return "%s://%s/%s" % (
-        saymon.settings.GRAPHITE_SCHEMA,
+        settings.GRAPHITE_SCHEMA,
         graphite_host,
         path
     )
 
 
 def list_metrics(*args):
+    if args:
+        print("Arguments not needed here")
+        exit(64)
+
     metrics_url = make_graphite_url("metrics/index.json")
     try:
         response = requests.get(metrics_url)
@@ -53,47 +56,47 @@ def list_metrics(*args):
                 response.reason
             ))
     except ConnectionError as exc:
-        print("Connection failed to %s: %s" %(metrics_url, exc.message))
+        print("Connection failed to %s: %s" % (metrics_url, exc.message))
 
 
-def metrica_data(*args):
-    metrica_name = args[0]
-    opentsdb_name = "%s" % metrica_name
+def metric_data(*args):
+    source_name = args[0]
+    target_name = "%s" % source_name
     tags = []
-    if 1<len(args):
-        tags=args[1:]
-        if "as"==args[1]:
-            opentsdb_name=args[2]
+    if 1 < len(args):
+        tags = args[1:]
+        if "as" == args[1]:
+            target_name = args[2]
             tags = args[3:]
     else:
         pass
 
-    metrica_data_url = make_graphite_url("render/?&_salt=%s&target=%s&format=json" % (
+    metric_data_url = make_graphite_url("render/?&_salt=%s&target=%s&format=json" % (
         time.time(),
-        metrica_name
+        source_name
     ))
     try:
-        response = requests.get(metrica_data_url)
+        response = requests.get(metric_data_url)
         if 200 == response.status_code:
             metrics_json = response.json()
-            if "datapoints" in metrics_json[0]:
+            if 'datapoints' in metrics_json[0]:
                 datapoints = metrics_json[0]["datapoints"]
                 for dp in datapoints:
                     print("%s %s %s %s" % (
-                        opentsdb_name, dp[1], dp[0], " ".join(tags)
+                        target_name, dp[1], dp[0], " ".join(tags)
                     ))
             else:
                 print("Unexpected response body: no datapoints key")
                 exit(64)
         else:
             print("Unexpected response code\n: GET %s\n RESULT %s %s" % (
-                metrica_data_url,
+                metric_data_url,
                 response.status_code,
                 response.reason
             ))
             exit(64)
     except ConnectionError as exc:
-        print("Connection failed to %s: %s" %(metrica_data_url, exc.message))
+        print("Connection failed to %s: %s" % (metric_data_url, exc.message))
         exit(64)
 
 if len(sys.argv) == 1:
@@ -101,13 +104,10 @@ if len(sys.argv) == 1:
     exit(0)
 else:
     cmd = sys.argv[1]
-    commands = dict(
-        list = list_metrics,
-        get = metrica_data,
-    )
+    commands = {
+        "list": list_metrics,
+        "get": metric_data
+    }
     if cmd in commands:
-        args = [x for x in sys.argv[2:]]
-        commands[cmd](*args)
-
-
-
+        func_args = [x for x in sys.argv[2:]]
+        commands[cmd](*func_args)
